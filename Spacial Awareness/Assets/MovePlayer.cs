570cb3da061ffceea03e6be1558e6f79034dc095
JaegerJochimsen <jaegerjochimsen@gmail.com>
2021-04-16 11:27:39 -0700

    Terrain setup and movement
    - Added and set up terrain asset
    - Began work on movement script for astronaut

---
+++ Spacial Awareness/Assets/MovePlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    public float speed = 15f;
+    private Animator anim;
+    private Rigidbody body;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        anim = gameObject.GetComponentInChildren<Animator>();
+        body.centerOfMass = new Vector3(0f, 0.1f, 0f) ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        bool hasInput = (horizontal != 0f || vertical != 0f);
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+        //bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        //bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        //bool isRunning = hasHorizontalInput || hasVerticalInput;
+        //anim.SetBool("IsRunning", isRunning);
+
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+        body.MoveRotation(m_Rotation);
+        body.MovePosition(transform.position + m_Movement * Time.deltaTime * speed);
+        
+    }
+
+
+}
