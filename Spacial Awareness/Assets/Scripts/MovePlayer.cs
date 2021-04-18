b110e7232723a5020f07537a5baddb80d9fbebe0
JD Paul <56490335+jdpaul123@users.noreply.github.com>
2021-04-17 18:02:44 -0700

    Added health bar and once it goes to 0 there is a debug that says player must die

--- Spacial Awareness/Assets/Scripts/MovePlayer.cs
+++ Spacial Awareness/Assets/Scripts/MovePlayer.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    // Oxigen/Health Level
+    public int maxHealth = 100;
+    public float currentHealth;
+
+    public HealthBar healthBar;
+    public float healthDecayCoef;
+    // End health Variables
+    private float timeStart = 0;
+
     public float turnSpeed = 15f;
     public float speed = 10f;
     Vector3 m_Movement;
@@ -23,6 +32,10 @@ public class MovePlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Health start
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        
         body = GetComponent<Rigidbody>();
         anim = gameObject.GetComponentInChildren<Animator>();
         body.centerOfMass = new Vector3(0f, 0.1f, 0f);
@@ -64,6 +77,7 @@ public class MovePlayer : MonoBehaviour
         }
 
         // jump if press space and we have something to jump from
+        // TODO BUG once you get on a platform you cannot jump again becuase player is not on ground
         if (Input.GetKeyDown("space") && isGrounded)
         {
             Jump();
@@ -74,6 +88,15 @@ public class MovePlayer : MonoBehaviour
         m_Rotation = Quaternion.LookRotation(desiredForward);
         body.MoveRotation(m_Rotation);
         body.MovePosition(transform.position + m_Movement * Time.deltaTime * speed);
+
+        // Change health over time
+        TakeDamage(healthDecayCoef * Time.deltaTime);
+        // Kill the player when health is 0
+        float health = healthBar.getHealth();
+        if (health < 1f)
+        {
+            Debug.Log("Player must die now");
+        }
         
     }
 
@@ -81,5 +104,12 @@ public class MovePlayer : MonoBehaviour
     {
         Vector3 movement = new Vector3(0.0f, 3.3f, 0.0f);
         body.AddForce(movement * speed, ForceMode.Impulse);
+    }
+
+    void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth((int)currentHealth);
     }
 }
