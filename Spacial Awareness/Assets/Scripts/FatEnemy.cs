3d1b10613a867f942e2686c05019e37ba97796f6
Sam Gebhardt <64874340+Sam-Gebhardt@users.noreply.github.com>
2021-04-18 21:07:14 -0700

    Enemy
    -Added stationary enemy
    -Added enemy that chases the player

---
+++ Spacial Awareness/Assets/Scripts/FatEnemy.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatEnemy : MonoBehaviour
+{
+
+    private Vector3 playerPos;
+    private GameObject player;
+    public float range;
+    private Rigidbody playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GameObject.Find("Stylized Astronaut").GetComponent<Rigidbody>();
+        player = GameObject.Find("Stylized Astronaut");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        playerPos = GameObject.Find("Stylized Astronaut").transform.position;
+        Vector3 DogPos = transform.position;
+
+        DogPos -= playerPos;
+        //Debug.Log(DogPos.x);
+
+        var x = range >= DogPos.x && DogPos.x >= -range;
+        var y = range >= DogPos.y && DogPos.y >= -range;
+        var z = range >= DogPos.z && DogPos.z >= -range;
+
+        // Player is within attack range of Enemy
+        if (x && y && z)
+        {
+            Attack(DogPos);
+        }
+        
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+       
+    }
+
+    // Call the interface for doing damage to the player from KillPlayer.cs 
+    void Attack(Vector3 knockback_dir)
+    {
+
+        KillPlayer health = player.GetComponent<KillPlayer>();
+        health.TakeDamage(1f);
+
+        // -1 is so Player goes away from enemy instead of towards
+        playerBody.AddForce(knockback_dir * 3 * -1, ForceMode.Impulse);
+       
+        // TODO: Add text to the screen
+    }
+}
+
+
+/* 
+ * 3 Classes of Enemy:
+ * 1: Fast + weak Damage
+ * 2: Medium speed + medium Damage
+ * 3: Stationary + Large Damage
+ * Knock Back
+ * 
+ */
