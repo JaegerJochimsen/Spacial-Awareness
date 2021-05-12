80fdf125c7b860f2c56af56f0d4a0f42f89b1752
JD Paul <56490335+jdpaul123@users.noreply.github.com>
2021-05-11 22:34:55 -0700

    Added flash and more lights

--- Spacial Awareness/Assets/Scripts/MoveScene.cs
+++ Spacial Awareness/Assets/Scripts/MoveScene.cs
@@ -5,16 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MoveScene : MonoBehaviour
 {
+    [SerializeField] FlashImage _flashImage = null;
     public string nextScene;
 
     //void OnCollisionEnter(Collision other)
     void OnTriggerEnter(Collider other)
     {
-       
+        _flashImage.StartFlash(.25f, 1f, Color.green);
         if (other.gameObject.CompareTag("Player")) 
         {
-            SceneManager.LoadScene(nextScene);
-           
+            Invoke("NextLevel", 1.0f);  
         }
+    }
+
+    void NextLevel()
+    {
+        SceneManager.LoadScene(nextScene);
     }
 }
