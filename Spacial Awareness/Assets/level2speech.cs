708b84559a62cfe08908a3ea4ddc315e1bd0a6b2
JD Paul <56490335+jdpaul123@users.noreply.github.com>
2021-05-23 15:47:32 -0700

    on screen text should be working next push

---
+++ Spacial Awareness/Assets/level2speech.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class level2speech : MonoBehaviour
+{
+    public Collider IGotOut;
+
+    public Text myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IGotOut = GameObject.Find("Speech/IGotOut").GetComponent<Collider>();
+
+        myText = GameObject.Find("Canvas/Text").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("IGotOut"))
+        {
+            myText.text = "Woo! I got out. Now where the heck am I?";
+            Debug.Log("I AM OUT!");
+        }
+    }
+}
