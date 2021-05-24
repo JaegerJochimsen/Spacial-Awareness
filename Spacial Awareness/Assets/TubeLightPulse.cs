88cdf59ec13efb65fab0a8a61ccc44e0585be45f
JaegerJochimsen <jaegerjochimsen@gmail.com>
2021-05-24 15:41:46 -0700

    Added battery and effect assets

--- Spacial Awareness/Assets/TubeLightPulse.cs
+++ Spacial Awareness/Assets/TubeLightPulse.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class TubeLightPulse : MonoBehaviour
 {
     // Start is called before the first frame update
+    Light current;
+    GameObject[] lights;
+    private float flickerMod;
+
     void Start()
     {
-        
+        if (lights == null)
+        {
+            lights = GameObject.FindGameObjectsWithTag("TubeLight");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // want the range of the lights to "pulse between 7 and 15, this rangedModifier will oscillate between 1 and 2
+        // so we just need to make our default range 7.5
+
+        foreach (GameObject light in lights)
+        {
+            flickerMod = Random.value;
+            current = light.GetComponentInChildren<Light>();
+            current.intensity = 3f * flickerMod;
+        }
     }
 }
