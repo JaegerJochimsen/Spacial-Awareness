0044f8e03f16499c4705a6c085e889d7089b7f0c
JaegerJochimsen <jaegerjochimsen@gmail.com>
2021-05-28 12:23:18 -0700

    Added post processing
    - Added screen space reflection
    - Began to implement screen zoom for end of level effect

--- Spacial Awareness/Assets/Scripts/MoveScene.cs
+++ Spacial Awareness/Assets/Scripts/MoveScene.cs
@@ -2,24 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.PostProcessing;
 
 public class MoveScene : MonoBehaviour
 {
     [SerializeField] FlashImage _flashImage = null;
     public string nextScene;
+    public PostProcessVolume volume;
+    public float changeValue;
 
     //void OnCollisionEnter(Collision other)
     void OnTriggerEnter(Collider other)
     {
-        _flashImage.StartFlash(.25f, 1f, Color.green);
+        LensDistortion lensDist;
+        volume.profile.TryGetSettings(out lensDist);
+        while(lensDist.intensity > -100f)
+        {
+            lensDist.intensity.value -= changeValue*Time.deltaTime;
+            //volume.profile.settings
+        }
+
+
+        _flashImage.StartFlash(.25f, 1f, Color.white);
         if (other.gameObject.CompareTag("Player")) 
         {
             Invoke("NextLevel", 1.0f);  
         }
     }
 
+
     void NextLevel()
     {
         SceneManager.LoadScene(nextScene);
     }
 }
+
+
