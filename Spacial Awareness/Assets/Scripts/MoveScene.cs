ce334cbfff7b3775dba9fa99a8d8d635cbf831c0
JaegerJochimsen <jaegerjochimsen@gmail.com>
2021-05-28 16:15:03 -0700

    Post Process Warp effect
    - Added post-process to exit

--- Spacial Awareness/Assets/Scripts/MoveScene.cs
+++ Spacial Awareness/Assets/Scripts/MoveScene.cs
@@ -2,27 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Rendering.PostProcessing;
 
 public class MoveScene : MonoBehaviour
 {
     [SerializeField] FlashImage _flashImage = null;
     public string nextScene;
-    public PostProcessVolume volume;
-    public float changeValue;
 
     //void OnCollisionEnter(Collision other)
     void OnTriggerEnter(Collider other)
     {
-        LensDistortion lensDist;
-        volume.profile.TryGetSettings(out lensDist);
-        while(lensDist.intensity > -100f)
-        {
-            lensDist.intensity.value -= changeValue*Time.deltaTime;
-            //volume.profile.settings
-        }
-
-
         _flashImage.StartFlash(.25f, 1f, Color.white);
         if (other.gameObject.CompareTag("Player")) 
         {
